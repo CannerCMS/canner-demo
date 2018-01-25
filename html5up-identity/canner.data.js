6bb2755e09f206941da503ec63f408f93a1d6600
chilijung <chilijung@gmail.com>
2018-01-25 19:38:32 +0800

    update data

--- html5up-identity/canner.data.js
+++ html5up-identity/canner.data.js
@@ -2,8 +2,8 @@ module.exports = {
   "main": {
     "name": "Mr. Bean",
     "description": "I have always worried about things more than I should.",
-    "thumb": imageService.image("public/images/avatar.png"),
-    "bg": imageService.image("public/images/bg.jpg"),
+    "thumb": imageService.image("public/images/avatar-upload.png"),
+    "bg": imageService.image("public/images/bg-upload.jpg"),
     "twitter": "https://twitter.com/mrbean",
     "instagram": "https://www.instagram.com/mrbean/",
     "facebook": "https://www.facebook.com/MrBean/"
